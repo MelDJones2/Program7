9983e8946440a9fdd21225475bbd7a86f2fac4d2
Mel Jones <121836871+MelJ03@users.noreply.github.com>
2024-04-22 17:32:46 -0500

    Add files via upload

---
+++ P07-Melvin/P07-Melvin/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace P07_Melvin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_Name = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblTest1 = new System.Windows.Forms.Label();
+            this.lblTest2 = new System.Windows.Forms.Label();
+            this.lblTest3 = new System.Windows.Forms.Label();
+            this.txt_Earned1 = new System.Windows.Forms.TextBox();
+            this.txt_Earned2 = new System.Windows.Forms.TextBox();
+            this.txt_Earned3 = new System.Windows.Forms.TextBox();
+            this.txt_Possible1 = new System.Windows.Forms.TextBox();
+            this.txt_Possible2 = new System.Windows.Forms.TextBox();
+            this.txt_Possible3 = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(480, 47);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Student Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(107, 218);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Points Earned";
+            // 
+            // txt_Name
+            // 
+            this.txt_Name.Location = new System.Drawing.Point(171, 92);
+            this.txt_Name.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_Name.Name = "txt_Name";
+            this.txt_Name.Size = new System.Drawing.Size(744, 22);
+            this.txt_Name.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(107, 316);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Points Possible";
+            // 
+            // lblTest1
+            // 
+            this.lblTest1.AutoSize = true;
+            this.lblTest1.Location = new System.Drawing.Point(335, 167);
+            this.lblTest1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTest1.Name = "lblTest1";
+            this.lblTest1.Size = new System.Drawing.Size(44, 16);
+            this.lblTest1.TabIndex = 4;
+            this.lblTest1.Text = "Test 1";
+            // 
+            // lblTest2
+            // 
+            this.lblTest2.AutoSize = true;
+            this.lblTest2.Location = new System.Drawing.Point(600, 167);
+            this.lblTest2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTest2.Name = "lblTest2";
+            this.lblTest2.Size = new System.Drawing.Size(44, 16);
+            this.lblTest2.TabIndex = 5;
+            this.lblTest2.Text = "Test 2";
+            // 
+            // lblTest3
+            // 
+            this.lblTest3.AutoSize = true;
+            this.lblTest3.Location = new System.Drawing.Point(868, 167);
+            this.lblTest3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTest3.Name = "lblTest3";
+            this.lblTest3.Size = new System.Drawing.Size(44, 16);
+            this.lblTest3.TabIndex = 6;
+            this.lblTest3.Text = "Test 3";
+            // 
+            // txt_Earned1
+            // 
+            this.txt_Earned1.Location = new System.Drawing.Point(288, 218);
+            this.txt_Earned1.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_Earned1.Name = "txt_Earned1";
+            this.txt_Earned1.Size = new System.Drawing.Size(132, 22);
+            this.txt_Earned1.TabIndex = 7;
+            // 
+            // txt_Earned2
+            // 
+            this.txt_Earned2.Location = new System.Drawing.Point(557, 218);
+            this.txt_Earned2.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_Earned2.Name = "txt_Earned2";
+            this.txt_Earned2.Size = new System.Drawing.Size(132, 22);
+            this.txt_Earned2.TabIndex = 8;
+            // 
+            // txt_Earned3
+            // 
+            this.txt_Earned3.Location = new System.Drawing.Point(816, 214);
+            this.txt_Earned3.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_Earned3.Name = "txt_Earned3";
+            this.txt_Earned3.Size = new System.Drawing.Size(132, 22);
+            this.txt_Earned3.TabIndex = 9;
+            // 
+            // txt_Possible1
+            // 
+            this.txt_Possible1.Location = new System.Drawing.Point(288, 308);
+            this.txt_Possible1.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_Possible1.Name = "txt_Possible1";
+            this.txt_Possible1.Size = new System.Drawing.Size(132, 22);
+            this.txt_Possible1.TabIndex = 10;
+            // 
+            // txt_Possible2
+            // 
+            this.txt_Possible2.Location = new System.Drawing.Point(557, 308);
+            this.txt_Possible2.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_Possible2.Name = "txt_Possible2";
+            this.txt_Possible2.Size = new System.Drawing.Size(132, 22);
+            this.txt_Possible2.TabIndex = 11;
+            // 
+            // txt_Possible3
+            // 
+            this.txt_Possible3.Location = new System.Drawing.Point(816, 308);
+            this.txt_Possible3.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_Possible3.Name = "txt_Possible3";
+            this.txt_Possible3.Size = new System.Drawing.Size(132, 22);
+            this.txt_Possible3.TabIndex = 12;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(171, 393);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(100, 28);
+            this.btnAdd.TabIndex = 13;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnFind
+            // 
+            this.btnFind.Location = new System.Drawing.Point(392, 393);
+            this.btnFind.Margin = new System.Windows.Forms.Padding(4);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(100, 28);
+            this.btnFind.TabIndex = 14;
+            this.btnFind.Text = "Find";
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(603, 393);
+            this.btnUpdate.Margin = new System.Windows.Forms.Padding(4);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(100, 28);
+            this.btnUpdate.TabIndex = 15;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(816, 393);
+            this.btnRemove.Margin = new System.Windows.Forms.Padding(4);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(100, 28);
+            this.btnRemove.TabIndex = 16;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(475, 465);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(4);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(105, 33);
+            this.btnClear.TabIndex = 17;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnFind);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txt_Possible3);
+            this.Controls.Add(this.txt_Possible2);
+            this.Controls.Add(this.txt_Possible1);
+            this.Controls.Add(this.txt_Earned3);
+            this.Controls.Add(this.txt_Earned2);
+            this.Controls.Add(this.txt_Earned1);
+            this.Controls.Add(this.lblTest3);
+            this.Controls.Add(this.lblTest2);
+            this.Controls.Add(this.lblTest1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_Name);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Grades";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_Name;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblTest1;
+        private System.Windows.Forms.Label lblTest2;
+        private System.Windows.Forms.Label lblTest3;
+        private System.Windows.Forms.TextBox txt_Earned1;
+        private System.Windows.Forms.TextBox txt_Earned2;
+        private System.Windows.Forms.TextBox txt_Earned3;
+        private System.Windows.Forms.TextBox txt_Possible1;
+        private System.Windows.Forms.TextBox txt_Possible2;
+        private System.Windows.Forms.TextBox txt_Possible3;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ P07-Melvin/P07-Melvin/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace P07_Melvin
+{
+    public partial class Form1 : Form
+    {
+        //Structure for TestScores info
+        struct TestScores
+        {
+            public string Name;
+            public int Earned_1;
+            public int Earned_2;
+            public int Earned_3;
+            public int Possible_1;
+            public int Possible_2;
+            public int Possible_3;
+        }
+
+        //Dictionary for TestScores info
+        Dictionary<string, TestScores> studentDictionary = new Dictionary<string, TestScores>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void ClearTextBoxes()
+        {
+            txt_Name.Text = "";
+            txt_Earned1.Text = "";
+            txt_Possible1.Text = "";
+            txt_Earned2.Text = "";
+            txt_Possible2.Text = "";
+            txt_Earned3.Text = "";
+            txt_Possible3.Text = "";
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Taking in Add Values ToInt32 to get Intergers from characters
+                string name = txt_Name.Text;
+                int test1Earned = int.Parse(txt_Earned1.Text);
+                int test2Earned = int.Parse(txt_Earned2.Text);
+                int test3Earned = int.Parse(txt_Earned3.Text);
+                int test1Possible = int.Parse(txt_Possible1.Text);
+                int test2Possible = int.Parse(txt_Possible2.Text);
+                int test3Possible = int.Parse(txt_Possible3.Text);
+                
+                TestScores studentData = new TestScores();
+                studentData.Name = txt_Name.Text;
+
+                if (test1Earned <= 0 || test1Earned > 100 ||
+                    test2Earned <= 0 || test2Earned > 100 ||
+                    test3Earned <= 0 || test3Earned > 100 ||
+                    test1Possible <= 0 || test1Possible > 100 ||
+                    test2Possible <= 0 || test2Possible > 100 ||
+                    test3Possible <= 0 || test3Possible > 100)
+                {
+                    MessageBox.Show("Invalid input for earned scores! Please enter valid integer values between 0 and 100.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+                if (!studentDictionary.ContainsKey(name))
+                {
+                    //Adding Info
+                    TestScores newTestScores;
+                    newTestScores.Name = name;
+                    newTestScores.Earned_1 = test1Earned;
+                    newTestScores.Earned_2 = test2Earned;
+                    newTestScores.Earned_3 = test3Earned;
+                    newTestScores.Possible_1 = test1Possible;
+                    newTestScores.Possible_2 = test2Possible;
+                    newTestScores.Possible_3 = test3Possible;
+
+                    studentDictionary.Add(name, newTestScores);
+
+                    ClearTextBoxes();
+                }
+                else
+                {
+                    //Error
+                    MessageBox.Show("Error: TestScores Already Exists!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string name = txt_Name.Text;
+
+                if (studentDictionary.ContainsKey(name))
+                {
+                    //Show info
+                    TestScores student = studentDictionary[name];
+                    txt_Earned1.Text = student.Earned_1.ToString();
+                    txt_Possible1.Text = student.Possible_1.ToString();
+                    txt_Earned2.Text = student.Earned_2.ToString();
+                    txt_Possible2.Text = student.Possible_2.ToString();
+                    txt_Earned3.Text = student.Earned_3.ToString();
+                    txt_Possible3.Text = student.Possible_3.ToString();
+                }
+                else
+                {
+                    //error
+                    MessageBox.Show("Error: TestScores Not Found!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string name = txt_Name.Text;
+
+                if (studentDictionary.ContainsKey(name))
+                {
+                    //Change Info for Name
+                    TestScores updatedTestScores = studentDictionary[name];
+                    updatedTestScores.Earned_1 = int.Parse(txt_Earned1.Text);
+                    updatedTestScores.Possible_1 = int.Parse(txt_Possible1.Text);
+                    updatedTestScores.Earned_2 = int.Parse(txt_Earned2.Text);
+                    updatedTestScores.Possible_2 = int.Parse(txt_Possible2.Text);
+                    updatedTestScores.Earned_3 = int.Parse(txt_Earned3.Text);
+                    updatedTestScores.Possible_3 = int.Parse(txt_Possible3.Text);
+
+                    studentDictionary[name] = updatedTestScores;
+                }
+                else
+                {
+                    //Error
+                    MessageBox.Show("Error: TestScores Not Found!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string name = txt_Name.Text;
+
+                //Removing name
+                if (studentDictionary.ContainsKey(name))
+                {
+                    studentDictionary.Remove(name);
+                }
+                else
+                {
+                    //Error
+                    MessageBox.Show("Error: TestScores Not Found!");
+                }
+
+                ClearTextBoxes();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            ClearTextBoxes();
+        }
+    }
+}
